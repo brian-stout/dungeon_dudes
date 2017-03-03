d6010b231902c6c32582fa61f06ba772d536e22b
Brian Stout <brianjonstout@gmail.com>
2017-03-03 09:04:06 -0500

    More framework for TDD and class entities.

--- dungeon_dudes.py
+++ dungeon_dudes.py
@@ -1,1 +1,15 @@
 #!usr/bin/env python3
+
+
+import entities.Creature as Creature
+import entities.Dice as Dice
+import entities.Dungeon as Dungeon
+import entities.Hero as Hero
+import entities.Loot as Loot
+import entities.Room as Room
+
+def main():
+    print("test")
+
+if __name__ == "__main__":
+    main()
--- dungeon_dudes_test.py
+++ dungeon_dudes_test.py
@@ -3,7 +3,27 @@ import dungeon_dudes as dungeon_dudes
 
 class TestFunction(unittest.TestCase):
 
-    def test_(self):
+    def test_object_Creature_creation(self):
+        creature = dungeon_dudes.entities.Creature()
+
+    def test_object_Hero_creation(self):
+        hero = dungeon_dudes.entities.Hero()
+
+    def test_object_Loot_creation(self):
+        loot = dungeon_dudes.entities.Loot()
+
+    def test_object_Dungeon_creation(self):
+        dungeon = dungeon_dudes.entities.Dungeon()
+
+    def test_object_Room_creation(self):
+        room = dungeon_dudes.entities.Room()
+
+    def test_object_Dice_creation(self):
+        dice = dungeon_dudes.entities.Dice()
+
+    ##def savestate
+
+    ##def menu?
         
         
 if __name__ == "__main__":
