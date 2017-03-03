0795edb3b0a5dbe1f251477c34da8825f763f568
Brian Stout <brianjonstout@gmail.com>
2017-03-03 08:54:15 -0500

    Setting up the basic files

---
+++ dungeon_dudes_test.py
@@ -0,0 +1,10 @@
+import unittest
+import dungeon_dudes as dungeon_dudes
+
+class TestFunction(unittest.TestCase):
+
+    def test_(self):
+        
+        
+if __name__ == "__main__":
+    unittest.main()
