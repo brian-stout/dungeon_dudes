0a1c7abcf70f946a39c71e9f1c07cd428a81bd2d
Brian Stout <brianjonstout@gmail.com>
2017-03-03 12:14:29 -0500

    Add inheritance to creature class for hero class.  super().__init__ for hero class.

--- entities/hero.py
+++ entities/hero.py
@@ -1,7 +1,7 @@
 from entities.creature import Creature
 
 class Hero(Creature):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        super().__init__(0)
 
 if __name__ == '__main__': pass
